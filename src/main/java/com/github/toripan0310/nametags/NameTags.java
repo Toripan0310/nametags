90f29dd77289a109c85eff308d2e6d13c37c2def
toripan8320 <224005416+Toripan0310@users.noreply.github.com>
2026-05-16 19:55:43 +0900

    Initial commit

---
+++ src/main/java/com/github/toripan0310/nametags/NameTags.java
@@ -0,0 +1,17 @@
+package com.github.toripan0310.nametags;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NameTags implements ModInitializer {
+	public static final String MOD_ID = "nametags";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/com/github/toripan0310/nametags/NameTagsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.github.toripan0310.nametags;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NameTagsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
