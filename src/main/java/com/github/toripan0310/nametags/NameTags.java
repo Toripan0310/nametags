fe8c85d38181733abd031058f0f169ebcb3cdbbc
toripan8320 <224005416+Toripan0310@users.noreply.github.com>
2026-05-16 22:45:25 +0900

    試作3 var2.2まで進んだ。コード大幅改変のためコミット

--- src/main/java/com/github/toripan0310/nametags/NameTags.java
+++ src/main/java/com/github/toripan0310/nametags/NameTags.java
@@ -1,16 +1,16 @@
 package com.github.toripan0310.nametags;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NameTags implements ModInitializer {
+public class NameTags implements ClientModInitializer {
 	public static final String MOD_ID = "nametags";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 
 		LOGGER.info("Player Name Tag Glowing!!!!!");
 	}
